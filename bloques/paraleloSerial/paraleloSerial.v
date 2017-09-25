259d5408711e737f9a84ab7008656199944343bf
Emilio Rojas <emilio93@gmail.com>
2017-09-25 07:59:41 -0600

    checkpoint ps sp

--- bloques/paraleloSerial/paraleloSerial.v
+++ bloques/paraleloSerial/paraleloSerial.v
@@ -1,12 +1,42 @@
 `timescale 1ns/1ps
 
-module paraleloSerial(entradas, salida, clk);
+// Este módulo se encarga de serializar una entrada paralela
+// tomada cada n ciclos de reloj
+//
+// Lo que hace es almacenar en un arreglo 'bits' de n-1 wires
+// la entrada tamada caada n ciclos de reloj.
+//
+// De este arreglo se entrega ordenadamente cada uno de sus
+// elementos cada ciclo de reloj, luego de entregar el último
+// elemento debe asignar a 'bits' las entradas, pero también
+// debe entregar la primera entrada que aun no está en 'bits',
+// se entrega la entrada directamente.
+//
+// Nota: precisamente por el hecho mencionado sobre lo que
+// ocurre luego de cada n ciclo de reloj, el elemento de
+// 'bits' que almacena esta entrada no es necesario, por
+// lo que puede no utilizarse, reduciendo el consumo de
+// potencia.
+//
+// Queda pendiente probar con la síntesis de yosys si al
+// optimizar el diseño, el consumo permanece igual.
+module paraleloSerial(entradas, salida, clk, rstContador);
 
+// esto es n en la explicación del módulo
 parameter cantidadBits = 10;
 
+// Se tienen n entradas
 input wire [cantidadBits-1:0] entradas;
+
+// La salida es serial, por lo tanto es un solo elemento
 output reg salida;
+
 input wire clk;
+
+// Indica que el contador se asigna a 0, esto es útil
+// en el probador para inicializar el valor de contador
+// puesto que inicia en un estado desconocido
+input wire rstContador;
 
 // Se utiliza para mantener la entrada
 // cada vez que esta entra(cada 10 flancos
@@ -15,16 +45,41 @@ input wire clk;
 // transcurso de esos flancos
 reg [cantidadBits-1:0] bits;
 
-reg contador = 0;
+// El modulo se encarga de identificar cuando
+// debe reiniciar el contador a partir de una
+// cuenta de ciclos de reloj
+// El reg contador se encarga de llevar la
+// cuenta de ciclos de reloj.
+//
+// Pendiente se debe usar el redondeo a entero
+// hacia arriba de log(n)
+reg [cantidadBits-1:0] contador;
 
+// Únicamente se realizan operaciones no bloqueantes
+// por lo que se utiliza un bloque que se ejecuta cada
+// flanco positivo de reloj
 always @(posedge clk) begin
-	contador = contador + 1;
-	salida <= bits[contador];
-	if(contador == cantidadBits) begin
-		contador = 0;
+
+	// Cuando rstContador está encendido se
+	// asigna 0 al contador, se obtienen las
+	// entradas y se envia la primer salida
+	if (rstContador) begin
+		contador <= 0;
 		bits <= entradas;
 		salida <= entradas[0];
+
+	end else begin
+		if (contador == 0) begin
+			salida <= entradas[1];
+			contador <= contador + 1;
+		end else if (contador < (cantidadBits-1)) begin
+			salida <= bits[contador+1];
+			contador <= contador + 1;
+		end else begin
+			contador <= 0;
+			bits <= entradas;
+			salida <= entradas[0];
+		end
 	end
 end
-
 endmodule
--- bloques/paraleloSerial/serialParalelo.v
+++ bloques/paraleloSerial/serialParalelo.v
@@ -1,25 +1,50 @@
 `timescale 1ns/1ps
 
-module serialParalelo(entrada, salidas, clk);
+// Este módulo se encarga de escuchar n bits en n ciclos
+// de reloj, y enviarlos una vez que se han adquirido los
+// n bits.
+//
+// Para llevar la cuenta del bit, se utiliza un contador
+// de ciclos de reloj
+module serialParalelo(entrada, salidas, clk, rstContador);
 
 parameter cantidadBits = 10;
 
+// El bit que se lee
 input wire entrada;
+
+// el arreglo que almacena los bits durante los n ciclos de
+// reloj que se almacenan los valores.
 output reg [cantidadBits-1:0] salidas;
 input wire clk;
+input wire rstContador;
 
+// Se utiliza para mantener la entrada
+// cada vez que esta entra(cada 10 flancos
+// positivos de reloj) independientemente
+// de que esta señal de entrada cambie en el
+// transcurso de esos flancos
 reg [cantidadBits-1:0] bits;
 
-reg contador = 0;
+reg [cantidadBits-1:0] contador;
 
 always @(posedge clk) begin
-	bits[contador] <= entrada;
-	contador <= contador + 1;
-	if(contador == cantidadBits) begin
+	if (rstContador) begin
 		contador <= 0;
-		salidas <= bits;
-		salidas[cantidadBits-1] <= entrada;
+		bits[0] <= entrada;
+	end
+	if (!rstContador) begin
+		if (contador == 0) begin
+			bits[1] <= entrada;
+			contador <= contador + 1;
+		end else if (contador < (cantidadBits-1)) begin
+			bits[contador+1] <= entrada;
+			contador <= contador + 1;
+		end else begin
+			contador <= 0;
+			salidas <= bits;
+			bits[0] <= entrada;
+		end
 	end
 end
-
 endmodule
