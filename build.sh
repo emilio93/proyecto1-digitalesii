1804fa7b4af66ef2082c52780e262de6e78f0b77
Emilio Rojas <emilio93@gmail.com>
2017-09-24 22:05:14 -0600

    actualizar build
    esto ya no se usa

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
-mkdir -p build
-mkdir -p gtkws
-cd bloques
-iverilog paraleloSerial_tb.v -o ../build/paraleloSerial_tb
-cd ..
-vvp -M ~/.local/install/ivl/lib/ivl build/paraleloSerial_tb
+curDir="$(pwd)"
+echo ${curDir}
+
+mkdir -p ${curDir}/build
+mkdir -p ${curDir}/gtkws
+
+
+cd ${curDir}/bloques
+iverilog ${curDir}/paraleloSerial_tb.v -o ${curDir}/build/paraleloSerial_tb
+cd ${curDir}
+vvp -M ~/.local/install/ivl/lib/ivl ${curDir}/build/paraleloSerial_tb
