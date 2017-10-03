f7a3d17cb97607caed525285073f9a1da3bba48a
Emilio Rojas <emilio93@gmail.com>
2017-10-02 21:05:01 -0600

    update serialParalelo

--- pruebas/testSerialParalelo.v
+++ pruebas/testSerialParalelo.v
@@ -12,177 +12,155 @@ module testSerialParalelo ();
 
 reg clk;
 reg enb;
-reg rstContador;
+reg rst;
 
 reg entrada;
-reg [9:0] esperado; // valor esperado en paralelo
 wire [9:0] salidas;
-wire [9:0] salidasSynth;
 
 wire clk10;
-wire clk10Synth;
 wire clk20;
-wire clk20Synth;
 wire clk40;
+
+wire [9:0] salidasSynth;
+wire clk10Synth;
+wire clk20Synth;
 wire clk40Synth;
 
 clks clkGen(
-  .clk(clk),
-  .clk10(clk10),
-  .clk20(clk20),
-  .clk40(clk40),
-  .rst(rstContador),
-  .enb(enb)
+  .clk(clk), .rst(rst), .enb(enb),
+  .clk10(clk10), .clk20(clk20), .clk40(clk40)
 );
 
 clksSynth clkGenSynth(
-  .clk(clk),
-  .clk10(clk10Synth),
-  .clk20(clk20Synth),
-  .clk40(clk40Synth),
-  .rst(rstContador),
-  .enb(enb)
+  .clk(clk), .rst(rst), .enb(enb),
+  .clk10(clk10Synth), .clk20(clk20Synth), .clk40(clk40Synth)
 );
 
 serialParalelo receptor(
-  .clk(clk),
-  .rst(rstContador),
-  .enb(enb),
+  .clk(clk), .rst(rst), .enb(enb),
   .clk10(clk10),
-	.entrada(entrada),
-	.salidas(salidas)
+  .entrada(entrada),
+  .salidas(salidas)
 );
 
 serialParaleloSynth receptorSynth(
-  .clk(clk),
-  .rst(rstContador),
-  .enb(enb),
+  .clk(clk), .rst(rst), .enb(enb),
   .clk10(clk10Synth),
-	.entrada(entrada),
-	.salidas(salidasSynth)
+  .entrada(entrada),
+  .salidas(salidasSynth)
 );
 
-integer periodo = 80;
-integer reloj = 40;
+always # 5 clk = ~clk;//inicio de la señal de reloj, cambia cada 10ns
 
-integer periodoGrande = 800;
-integer medioPeriodoGrande = 400;
-integer cuartoPeriodoGrande = 200;
-
-always # reloj clk = ~clk;//inicio de la señal de reloj, cambia cada 10ns
+reg [9:0] error;
+always @ ( * ) begin
+  error = salidas-salidasSynth;
+end
 
 initial begin
-	clk = 0;
-	entrada = 1'b1;
-  rstContador = 1'b1;
-  enb = 0;
+  $dumpfile("gtkws/testSerialParalelo.vcd");
+  $dumpvars;
+end
 
-  # periodo
+initial begin
+  clk <= 0;
+  entrada <= 1'b1;
+  rst <= 1'b1;
+  enb <= 0;
+
+  # 10
   @ (posedge clk);
-  entrada = 1'b0;
-  enb = 1;
+  entrada <= 1'b0;
+  enb <= 1;
 
-  # periodo
+  # 10
   @ (posedge clk);
-  entrada = 1'b0;
+  entrada <= 1'b0;
 
-  # periodo
+  # 10
   @ (posedge clk);
-  entrada = 1'b1;
-
+  entrada <= 1'b1;
 
   //
-  // SE APAGA rstContador
+  // SE APAGA rst
   //
-  # periodo
+  # 10
   @ (posedge clk);
-  rstContador = 1'b0;
-  entrada = 1'b1;
+  rst <= 1'b0;
+
+  // 0110 0110 10
+  @ (posedge clk10); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b0;
+
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b0;
+
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b0;
+
+  @ (posedge clk10); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b1;
+
+  @ (posedge clk10); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+
+  @ (posedge clk); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b0;
+
+  @ (posedge clk10); entrada <= 1'b0;
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b1;
+
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b1;
+
+  @ (posedge clk); entrada <= 1'b1;
+  @ (posedge clk); entrada <= 1'b1;
+
+  @ (posedge clk10);  entrada <= 1'b1;
+  @ (posedge clk10);  entrada <= 1'b0;
 
   @ (posedge clk10);
-  esperado = 10'b1011001100;
-  entrada = 1'b0;
-  # 1 @ (posedge clk);
-  entrada = 1'b1;
-  # 1 @ (posedge clk);
-  entrada = 1'b1;
-  # 1 @ (posedge clk);
-  entrada = 1'b0;
-  # 1 @ (posedge clk);
-  entrada = 1'b0;
-  # 1 @ (posedge clk);
-  entrada = 1'b1;
-  # 1 @ (posedge clk);
-  entrada = 1'b1;
-  # 1 @ (posedge clk);
-  entrada = 1'b0;
-  # 1 @ (posedge clk);
-  entrada = 1'b0;
+  rst <= 1'b1;
 
+  # 20
+  @ (posedge clk);
+  entrada <= 1'b0;
 
+  # 20
   @ (posedge clk);
-  entrada = 1'b1;
+  entrada <= 1'b1;
 
-
+  # 20
   @ (posedge clk);
-
-  entrada = 1'b0;
-  @ (posedge clk);
-  entrada = 1'b0;
-  @ (posedge clk);
-  esperado = 10'b0011001100;
-  entrada = 1'b1;
-  @ (posedge clk);
-  entrada = 1'b1;
-  @ (posedge clk);
-  entrada = 1'b0;
-  @ (posedge clk);
-  entrada = 1'b0;
-  @ (posedge clk);
-  entrada = 1'b1;
-  @ (posedge clk);
-  entrada = 1'b1;
-  @ (posedge clk);
-  entrada = 1'b0;
-  @ (posedge clk);
-  entrada = 1'b0;
-
-  # 400
-  @ (posedge clk);
-  entrada = 1'b1;
-
-
-
-  # 400
-  @ (posedge clk);
-  rstContador = 1'b1;
+  entrada <= 1'b1;
 
   # 50
-  @ (posedge clk);
-  entrada = 1'b0;
-
-  # 50
-  # periodoGrande
-  @ (posedge clk);
-  entrada = 1'b1;
-
-  # 50
-  @ (posedge clk);
-  entrada = 1'b1;
-
-	# 50
-	$finish;
-
+  $finish;
 end
-
-initial begin
-	$dumpfile("gtkws/testSerialParalelo.vcd");
-	$dumpvars;
-	$display("		tiempo    | clk | rstContador |   entradas    | salida | contadorE | timepo en escala usada");
-	$monitor("%t      | %b   | %b           | %b          | %f ns",
-		$time, clk, rstContador, entrada, salidas, $realtime);
-
-end
-
 
 endmodule
