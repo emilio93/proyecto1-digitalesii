826e8a2e9dd4117e2a7f69e13a0d7d0762073217
Robin <weiserfi@outlook.com>
2017-10-07 17:44:39 -0600

    encoder v2

--- bloques/encoder8-10/encoder.v
+++ bloques/encoder8-10/encoder.v
@@ -11,18 +11,14 @@ input wire clk;
 input wire K, enb;
 output reg [9:0] salidas;//wire no funca
 
-
 reg PDRS6, NDRS6, PDRS4, NDRS4;
 reg PDFS6, NDFS6, PDFS4, NDFS4; //ESTADO ANTERIOR
-reg alterno;
+reg alterno, DBn;
 reg L03, L30, L12, L21;
 reg A, B, C, D, E, F, G, H;
 
-//
-
 //H G F E D C B A
 //7 6 5 4 3 2 1 0
-
 
 //j h g f i e d c b a
 //9 8 7 6 5 4 3 2 1 0
@@ -31,20 +27,22 @@ reg A, B, C, D, E, F, G, H;
 
 parameter retardo = 10;
 
+//always @(clk)begin
+
 always @(negedge clk) begin
 
-	if(enb)begin
+//	alterno = 0;
+//	NDRS6 = 1;
+//	NDRS4 = 1;
+//	PDRS6 = 0;
+//	PDRS4 = 0;
 
-	NDRS6 = 1;
-	NDRS4 = 1;
-	PDRS6 = 0;
-	PDRS4 = 0;
+	if(enb)begin
 
 	L03 <= (~entradas[0])&(~entradas[1])&(~entradas[2]);
 	L30 <= (entradas[0])&(entradas[1])&(entradas[2]);
 	L12 <= (entradas[0]&(~entradas[1])&(~entradas[2]))|((~entradas[0])&entradas[1]&(~entradas[2]))|((~entradas[0])&(~entradas[1])&entradas[2]);
 	L21 <= (~entradas[0]&entradas[1]&entradas[2])|(entradas[0]&~entradas[1]&entradas[2])|(entradas[0]&entradas[1]&~entradas[2]);
-
 
 	A <= entradas[0];
 	B <= entradas[1];
@@ -57,40 +55,41 @@ always @(negedge clk) begin
 
 	#retardo;
 
-
 	//DIFERENTE: XOR ^
 	//IGUAL: XNOR -^
-
-	salidas[9] <= 	(F^G)&~H | F&G&H&K;//IGNORO SEÑAL SS
-	salidas[8] <= 	H;
-	salidas[7] <= 	G | ~F&~G&~H -^ G | ~F&~H;
-	salidas[6] <= 	F&~(F&G&H&K);//IGNORO S A PETICION DEL PROFESOR
-	salidas[5] <= 	L21&~D&~E | L12&((D ^ E) | K) | L03&D&~E | L30&D&E;
-	salidas[4] <= 	(E&~(L03&D)) | (L12&~D&~E) | (L03&D&~E);
-	salidas[3] <=	(D&~(L30&D));
-	salidas[2] <= 	C|(L30&~D) | L03&D&(E -^ C) | (L03&(~D|D&E)) -^ C | L03&(~D|E);
-	salidas[1] <= 	B&~(L30&D)|L03&~D;
-	salidas[0] <= 	A;
+	//Ingoro señal s como lo recomendó el profesor
+//	salidas[9] <= 	(F^G)&~H | F&G&H&K;	//j
+//	salidas[8] <= 	H;			//h
+//	salidas[7] <= 	G | ~F&~G&~H -^ G | ~F&~H;//g
+//	salidas[6] <= 	F&~(F&G&H&K);		//f
+	salidas[5] <= 	L21&~D&~E | L12&((D ^ E) | K) | L03&D&~E | L30&D&E;//i
+	salidas[4] <= 	(E&~(L03&D)) | (L12&~D&~E) | (L03&D&~E);//e
+	salidas[3] <=	(D&~(L30&D));		//d
+	salidas[2] <= 	C|(L30&~D) | L03&D&(E -^ C) | (L03&(~D|D&E)) -^ C | L03&(~D|E);//c
+	salidas[1] <= 	B&~(L30&D)|L03&~D;	//b
+	salidas[0] <= 	A;			//a
 
 	#retardo
 
-	
+//DR cambia menos que DB para inventir señal	
 //~
 	PDRS6 <= (L03&(D|~E)) | (L30&D&~E) | (L12&~D&~E);//Running Disparity
 	NDRS6 <= L30&(~D&E) |L03&~D&E | L21&D&E | K;
 	PDRS4 <= ~F&~G | (F ^ G)&K;
 	NDRS4 <= F&G;
 
-	if ((PDRS6 != NDRS6) & (PDRS4 != NDRS4)) alterno = 1;
-		else alterno = 0;
+//	if ((PDRS6 != NDRS6) & (PDRS4 != NDRS4)) alterno = 1;
+//		else alterno = 0;
 
-	if (alterno) salidas[9:0] = salidas[9:0] ^ (10'b1111111111);
+	DBn = L21&(~D|~E)|L30&~D&~E|L12&(D|E)&~K;
+	if(DBn) alterno = 0 ; else alterno = 1;
 
 	PDFS6 <= PDRS6;
 	PDFS4 <= PDRS4;
 	NDFS6 <= NDRS6;
 	NDFS4 <= NDRS4;
 
+	if (alterno) salidas[9:0] = salidas[9:0] ^ (10'b1111111111);
 end//if
 
 end
