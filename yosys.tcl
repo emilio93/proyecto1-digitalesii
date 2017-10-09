633b4c2c9ee83d49e2fb10a101fe3e07e8e6ab0a
Robin <weiserfi@outlook.com>
2017-10-08 21:04:29 -0600

    del en yosys

--- yosys.tcl
+++ yosys.tcl
@@ -7,41 +7,41 @@ set vlogFileName   "$::env(VLOG_FILE_NAME)"
 yosys read_verilog $vlogFileName
 
 hierarchy -check -top $vlogModuleName
-show -prefix pdfs/$vlogModuleName-original -format pdf -colors 3 -viewer echo
+#show -prefix pdfs/$vlogModuleName-original   -colors 3 -viewer echo
 
 yosys proc
-show -prefix pdfs/$vlogModuleName-proc -format pdf -colors 3 -viewer echo
+#show -prefix pdfs/$vlogModuleName-proc -colors 3 -viewer echo
 
 opt
-show -prefix pdfs/$vlogModuleName-proc_opt -format pdf -colors 3 -viewer echo
+#show -prefix pdfs/$vlogModuleName-proc_opt   -colors 3 -viewer echo
 
 fsm
-show -prefix pdfs/$vlogModuleName-fsm -format pdf -colors 3 -viewer echo
+#show -prefix pdfs/$vlogModuleName-fsm   -colors 3 -viewer echo
 
 opt
-show -prefix pdfs/$vlogModuleName-fsm_opt -format pdf -colors 3 -viewer echo
+#show -prefix pdfs/$vlogModuleName-fsm_opt   -colors 3 -viewer echo
 
 memory
-show -prefix pdfs/$vlogModuleName-memory -format pdf -colors 3 -viewer echo
+#show -prefix pdfs/$vlogModuleName-memory   -colors 3 -viewer echo
 
 opt
-show -prefix pdfs/$vlogModuleName-memory_opt -format pdf -colors 3 -viewer echo
+#show -prefix pdfs/$vlogModuleName-memory_opt   -colors 3 -viewer echo
 
 techmap
-# show -prefix pdfs/$vlogModuleName-techmap -format pdf -colors 3 -viewer echo
+# show -prefix pdfs/$vlogModuleName-techmap   -colors 3 -viewer echo
 
 opt
-# show -prefix pdfs/$vlogModuleName-techmap_opt -format pdf -colors 3 -viewer echo
+# show -prefix pdfs/$vlogModuleName-techmap_opt   -colors 3 -viewer echo
 
 write_verilog ./build/$vlogModuleName-rtlil.v
 
 dfflibmap -liberty ./lib/cmos_cells.lib
-# show -prefix pdfs/$vlogModuleName-dff_seq -lib ./lib/cmos_cells.v -format pdf -colors 3 -viewer echo
+# show -prefix pdfs/$vlogModuleName-dff_seq -lib ./lib/cmos_cells.v   -colors 3 -viewer echo
 
 abc -liberty ./lib/cmos_cells.lib
-# show -prefix pdfs/$vlogModuleName-abc_comb -lib ./lib/cmos_cells.v -format pdf -colors 3 -viewer echo
+# show -prefix pdfs/$vlogModuleName-abc_comb -lib ./lib/cmos_cells.v   -colors 3 -viewer echo
 
 clean
 
-# show -prefix pdfs/$vlogModuleName-synth -lib ./lib/cmos_cells.v -viewer echo -format pdf -colors 3 -viewer echo
+# show -prefix pdfs/$vlogModuleName-synth -lib ./lib/cmos_cells.v -viewer echo   -colors 3 -viewer echo
 write_verilog ./build/$vlogModuleName-sintetizado.v
